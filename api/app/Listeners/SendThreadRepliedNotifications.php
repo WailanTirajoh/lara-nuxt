40caad99bfbc42b8308caca6f7f8dacf73c91aa2
Wailan Tirajoh <wailantirajoh@gmail.com>
2023-10-29 01:15:24 +0700

    feat(perf): improve query, user groupBy instead of distinct

--- api/app/Listeners/SendThreadRepliedNotifications.php
+++ api/app/Listeners/SendThreadRepliedNotifications.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\ThreadReplied;
 use App\Models\User;
 use App\Notifications\ThreadReplied as NotificationsThreadReplied;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Notification;
 
@@ -18,12 +19,21 @@ class SendThreadRepliedNotifications
         );
     }
 
-    private function getAssociatedUsers(ThreadReplied $event)
+    private function getAssociatedUsers(ThreadReplied $event): Collection
     {
-        $userIds = $event->reply->replyable->replies()
+        $thread = $event->reply->replyable;
+        $userIds = $thread->replies()
             ->whereNot('user_id', Auth::id())
-            ->distinct('user_id')
-            ->pluck('user_id');
+            ->groupBy('user_id')
+            ->pluck('user_id')
+            ->when(
+                Auth::id() !== $thread->user_id,
+                function (Collection $userIds) use ($thread) {
+                    if (!$userIds->contains($thread->user_id)) {
+                        $userIds->push($thread->user_id);
+                    }
+                }
+            );
 
         return User::whereIn('id', $userIds)
             ->get();
